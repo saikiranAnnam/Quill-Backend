5c19e39c8c39065aab199f6919bda8e7381d8ec1
saikiranAnnam <saikiranannam23@gmail.com>
2024-07-18 18:47:33 +0530

    intialzed prisma client

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,31 +1,55 @@
 import { Hono } from 'hono'
-// import { User } from "@prisma/client";
+import { PrismaClient } from "@prisma/client/edge";
+import { withAccelerate } from "@prisma/extension-accelerate";
 
-const app = new Hono()
+const app = new Hono<{
+  Bindings: {
+    DATABASE_URL: string;
+  };
+}>();
 
-app.post('/api/v1/user/signup', (c:any) =>{
-  return c.text('Hello Hono !!')
-})
+app.post("/api/v1/user/signup", (c: any) => {
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate());
 
-app.post('/api/v1/user/signin', (c:any) =>{
-  return c.text('Hello Hono !!')
-})
+  return c.text("Hello Hono !!");
+});
 
-app.post('/api/v1/blog', (c:any) =>{
-  return c.text('Hello Hono !!')
-})
+app.post("/api/v1/user/signin", (c: any) => {
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate());
 
-app.put('/api/v1/blog', (c:any) =>{
-  return c.text('Hello Hono !!')
-  
-})
+  return c.text("Hello Hono !!");
+});
 
-app.get('/api/v1/blog/:id', (c:any) =>{
-  return c.text('Hello Hono !!')
-})
+app.post("/api/v1/blog", (c: any) => {
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate());
+  return c.text("Hello Hono !!");
+});
 
-app.get('/api/v1/blog/bulk', (c:any) =>{
-  return c.text('Hello Hono !!')
-})
+app.put("/api/v1/blog", (c: any) => {
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate());
+  return c.text("Hello Hono !!");
+});
+
+app.get("/api/v1/blog/:id", (c: any) => {
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate());
+  return c.text("Hello Hono !!");
+});
+
+app.get("/api/v1/blog/bulk", (c: any) => {
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate());
+  return c.text("Hello Hono !!");
+});
 
 export default app
